9e2accc802a9726497f2cf688e9a984db3e21a1b
Paaske7 <paaske7@hotmail.com>
2018-01-16 11:02:36 +0100

    .

--- 3SemesterEksamen/3SemesterEksamen/Controllers/IndexController.cs
+++ 3SemesterEksamen/3SemesterEksamen/Controllers/IndexController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using _3SemesterEksamen.Models;
+
 
 namespace _3SemesterEksamen.Controllers
 {
@@ -13,5 +15,44 @@ namespace _3SemesterEksamen.Controllers
         {
             return View();
         }
+        [HttpPost]
+        public ViewResult Index(Index poll)
+        {
+            poll.URL = "http://localhost:59124/Index/Next"; 
+            if (ModelState.IsValid)
+            {
+                return View("URL", poll);
+            }
+            else
+            {
+                return View();
+            }
+            
+        }
+        [HttpPost]
+        public ViewResult URL(Index poll)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Result", poll);
+            }
+            else
+            {
+                return View();
+            }
+            
+        }
+        public ActionResult Result()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult Result(Index poll)
+        {    
+            return View(); 
+        }
+
+      
+
     }
 }
--- 3SemesterEksamen/3SemesterEksamen/Models/Index.cs
+++ 3SemesterEksamen/3SemesterEksamen/Models/Index.cs
@@ -7,5 +7,15 @@ namespace _3SemesterEksamen.Models
 {
     public class Index
     {
+        public string Titel { get; set; }
+        public string Mulighed1 { get; set; }
+        public string Mulighed2 { get; set; }
+        public string Mulighed3 { get; set; }
+        public string URL { get; set; }
+        public bool Remember { get; set; }
+        public bool Remember2 { get; set; }
+        public bool Remember3 { get; set; }
+
+
     }
 }
